c0b1e3fc58f3c1e988497c6c7585a9969cc2575e
udaypratap1112 <udaypratapsinghsiondiya@gmail.com>
2024-08-04 09:00:23 +0530

    disable scroll

--- scroll.js
+++ scroll.js
@@ -1,19 +1,19 @@
-function disableScroll() {
-    // Get the current page scroll position
-    scrollTop =
-        window.scrollY ||
-        document.documentElement.scrollTop;
-    scrollLeft =
-        window.scrollX ||
-        document.documentElement.scrollLeft,
+// function disableScroll() {
+//     // Get the current page scroll position
+//     scrollTop =
+//         window.scrollY ||
+//         document.documentElement.scrollTop;
+//     scrollLeft =
+//         window.scrollX ||
+//         document.documentElement.scrollLeft,
 
-        // if any scroll is attempted,
-        // set this to the previous value
-        window.onscroll = function () {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}
-  disableScroll()
- setTimeout(() => {
-    window.onscroll = function () { };
- }, 2000);
+//         // if any scroll is attempted,
+//         // set this to the previous value
+//         window.onscroll = function () {
+//             window.scrollTo(scrollLeft, scrollTop);
+//         };
+// }
+//   disableScroll()
+//  setTimeout(() => {
+//     window.onscroll = function () { };
+//  }, 2000);
